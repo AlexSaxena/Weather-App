3c46a48bbe128074fe337132e4b5ab49e39633d1
AlexSaxena <saxenaalex@gmail.com>
2022-08-17 18:33:26 +0200

    Adds async func for getWeather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("Hello There!");
+const totallyHiddenKey = "a410c7747e64412717e2146de1d964f7";
+
+async function getWeather(location = "London") {
+  let response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${totallyHiddenKey}`,
+    { mode: "cors" }
+  );
+  let showWeather = await response.json();
+  console.log(showWeather);
+
+  return showWeather;
+}
+
+getWeather();
