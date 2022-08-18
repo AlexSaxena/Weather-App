4dac4263331869f3600fca944d8ba32d5d5e8625
AlexSaxena <saxenaalex@gmail.com>
2022-08-18 15:42:53 +0200

    Adds a high level main func.. that directs data

--- src/index.js
+++ src/index.js
@@ -6,10 +6,38 @@ async function getWeather(location = "London") {
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${totallyHiddenKey}`,
     { mode: "cors" }
   );
-  let showWeather = await response.json();
-  console.log(showWeather);
+  let getWeatherObj = await response.json();
+  // console.log("inside GetWeather func.. ");
+  // console.log(getWeatherObj);
+  // console.log(getWeatherObj.name);
+  // console.log(getWeatherObj.main);
+  // console.log(getWeatherObj.main.temp);
+  // console.log(getWeatherObj.main.temp_min);
+  // console.log(getWeatherObj.main.temp_max);
+  // console.log(getWeatherObj.main.feels_like);
+  // console.log(getWeatherObj.main.pressure);
+  // console.log(getWeatherObj.main.humidity);
+  // console.log(getWeatherObj.weather[0].main);
+  // console.log(getWeatherObj.weather[0].description);
 
-  return showWeather;
+  return getWeatherObj;
+}
+
+// Contorller function
+async function showWeather(location) {
+  let weather = await getWeather(location);
+  let wlocation = weather.name;
+  let wTemp = weather.main.temp;
+  let wTempMin = weather.main.temp_min;
+  let wTempMax = weather.main.temp_max;
+  let wFeelsLike = weather.main.feels_like;
+  let wPressure = weather.main.pressure;
+  let wHumidity = weather.main.humidity;
+  let wStatus = weather.weather[0].main;
+  let wStatusDesc = weather.weather[0].description;
+  console.log("From show Weather Func name: " + weather.name);
+
+  return console.log("Make a Card Func...");
 }
 
 getWeather();
